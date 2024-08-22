0a2fb92e82cdcc82bd76d50677152fe842c38986
Matias Eduardo Isaguirre Escobar <misaguir@c1r5s4.42malaga.com>
2024-08-22 18:02:17 +0200

    mimicroshell

--- microshell.c
+++ microshell.c
@@ -1,71 +1,69 @@
+#include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <string.h>
 #include <stdlib.h>
 
-void	ft_putstr_fd2(char *str, char *arg)
+void ft_putstrfd(char *str,char *arg)
 {
-	while (*str)
-		write(2, str++, 1);
-	if (arg)
+	while(*str)
+		write(2,str++,1);
+	if(arg)
 		while(*arg)
-			write(2, arg++, 1);
-	write(2, "\n", 1);
+			write(2,arg++,1);
+	write(2,"\n",1);
 }
-
-void ft_execute(char *argv[], int i, int tmp_fd, char *env[])
+void ft_exec(char **argv,char **envp,int i,int tmp_fd)
 {
 	argv[i] = NULL;
-	dup2(tmp_fd, 0);
+	dup2(tmp_fd,0);
 	close(tmp_fd);
-	execve(argv[0], argv, env);
-	ft_putstr_fd2("error: cannot execute ", argv[0]);
+	execve(argv[0],argv,envp);
+	ft_putstrfd("error: cannot execute ",argv[0]);
 	exit(1);
 }
 
-int	main(int argc, char *argv[], char *env[])
+int main(int argc, char **argv, char **envp)
 {
-	int	i;
-	int fd[2];
+	int i = 0;
 	int tmp_fd;
-	(void)argc;
+	int fd[2];
 
-	i = 0;
+	(void)argc;
 	tmp_fd = dup(0);
-	while (argv[i] && argv[i + 1])
+	while(argv[i] && argv[i + 1])
 	{
-		argv = &argv[i + 1];
+		argv = &argv[1 + i];
 		i = 0;
-		while (argv[i] && strcmp(argv[i], ";") && strcmp(argv[i], "|"))
+		while(argv[i] && strcmp(argv[i],";") && strcmp(argv[i],"|"))
 			i++;
-		if (strcmp(argv[0], "cd") == 0) //cd
+		if (strcmp(argv[0],"cd") == 0)
 		{
 			if (i != 2)
-				ft_putstr_fd2("error: cd: bad arguments", NULL);
+				ft_putstrfd("error: cd: bad arguments",NULL);
 			else if (chdir(argv[1]) != 0)
-				ft_putstr_fd2("error: cd: cannot change directory to ", argv[1]	);
+				ft_putstrfd("error: cd: cannot change directory to ",argv[1]);
 		}
-		else if (i != 0 && (argv[i] == NULL || strcmp(argv[i], ";") == 0))
+		else if (i != 0 && (argv [i] == NULL || strcmp(argv[i],";") == 0))
 		{
-			if ( fork() == 0)
-				ft_execute(argv, i, tmp_fd, env);
+			if(fork() == 0)
+				ft_exec(argv,envp,i,tmp_fd);
 			else
 			{
 				close(tmp_fd);
-				while(waitpid(-1, NULL, WUNTRACED) != -1)
+				while(waitpid(-1,NULL,2) != -1)
 					;
 				tmp_fd = dup(0);
 			}
 		}
-		else if(i != 0 && strcmp(argv[i], "|") == 0)
+		else if ( i != 0 && strcmp(argv[i],"|") == 0)
 		{
 			pipe(fd);
-			if ( fork() == 0)
+			if (fork() == 0)
 			{
-				dup2(fd[1], 1);
+				dup2(fd[1],1);
 				close(fd[0]);
 				close(fd[1]);
-				ft_execute(argv, i, tmp_fd, env);
+				ft_exec(argv,envp,i,tmp_fd);
 			}
 			else
 			{
@@ -74,7 +72,7 @@ int	main(int argc, char *argv[], char *env[])
 				tmp_fd = fd[0];
 			}
 		}
+
 	}
 	close(tmp_fd);
-	return (0);
 }
